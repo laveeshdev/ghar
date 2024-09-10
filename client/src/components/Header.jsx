e56366a9c113e9d973a71867f4bf0aa8fc950556
laveeshsanadhya <laveeshsanadhya@gmail.com>
2024-09-10 23:32:24 +0530

    decided a new name for the website from ghar dekho to HouseX

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -27,8 +27,8 @@ export default function Header() {
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to='/'>
           <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-slate-500'>Ghar</span>
-            <span className='text-slate-700'>Dekho</span>
+            <span className='text-slate-500'>House</span>
+            <span className='text-slate-700'>X</span>
           </h1>
         </Link>
         <form
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -59,7 +59,7 @@ export default function Home() {
           place with ease
         </h1>
         <div className='text-gray-400 text-xs sm:text-sm'>
-          Ghar Dekho is the best place to find your next perfect place to
+          HouseX is the best place to find your next perfect place to
           live.
           <br />
           We have a wide range of properties for you to choose from.
